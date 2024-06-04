ec2f95857c552db99b2ba71c54950042cac78a33
Zero <1203970284@qq.com>
2024-06-04 21:15:18 +0800

    chore: test to remove dynamic component

--- packages/configurator/frontend/src/LayoutV2.vue
+++ packages/configurator/frontend/src/LayoutV2.vue
@@ -1,18 +1,9 @@
 <script setup lang="tsx">
-import { NLayout, NLayoutContent, NLayoutHeader } from 'naive-ui'
+import { NLayout } from 'naive-ui'
 </script>
 
 <template>
   <NLayout>
-    <NLayoutHeader>Header</NLayoutHeader>
-    <NLayout has-sider>
-      <NLayoutSider>
-        侧边栏
-      </NLayoutSider>
-      <NLayoutContent>
-        内容
-      </NLayoutContent>
-    </NLayout>
-    <NLayoutFooter>Footer</NLayoutFooter>
+    hello world
   </NLayout>
 </template>
