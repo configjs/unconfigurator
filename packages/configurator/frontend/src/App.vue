7ee165c8a3f08fe9fcb530b01a8dda9c12646d63
Zero <1203970284@qq.com>
2024-06-04 20:34:28 +0800

    chore: test remove all provider

--- packages/configurator/frontend/src/App.vue
+++ packages/configurator/frontend/src/App.vue
@@ -1,20 +1,6 @@
 <script setup lang="tsx">
-import { NConfigProvider, NDialogProvider, NLoadingBarProvider, NMessageProvider, NNotificationProvider, darkTheme, zhCN } from 'naive-ui'
-import { isDark } from './composables/dark'
 </script>
 
 <template>
-  <Suspense>
-    <NConfigProvider :locale="zhCN" :theme="isDark ? darkTheme : null">
-      <NMessageProvider>
-        <NDialogProvider>
-          <NNotificationProvider>
-            <NLoadingBarProvider>
-              Hello world!
-            </NLoadingBarProvider>
-          </NNotificationProvider>
-        </NDialogProvider>
-      </NMessageProvider>
-    </NConfigProvider>
-  </Suspense>
+  Hello world!
 </template>
